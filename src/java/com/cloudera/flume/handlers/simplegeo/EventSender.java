18300a0fa00d32efe4dc00d6f449d0be94264a1f
dsmith <derek@simplegeo.com>
2011-03-20 21:10:20 -0700

    Generalize the Http request process.

--- src/java/com/cloudera/flume/handlers/simplegeo/EventSender.java
+++ src/java/com/cloudera/flume/handlers/simplegeo/EventSender.java
@@ -1,115 +1,105 @@
 package com.cloudera.flume.handlers.simplegeo;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.HttpHost;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.cloudera.flume.core.Event;
-import com.simplegeo.client.AbstractSimpleGeoClient;
-import com.simplegeo.client.callbacks.SimpleGeoCallback;
-import com.simplegeo.client.handler.SimpleGeoJSONHandler;
 
 public class EventSender {
 	public static final Logger LOG = LoggerFactory.getLogger(EventSender.class);
-	private StupidlySimpleGeoClient client;
+	private HttpClient client;
+	
+	private HttpHost host;
 	
 	public EventSender(String host, int port) {
-		this.client = new StupidlySimpleGeoClient(host, port);
+		this.client = new DefaultHttpClient();
+		this.host = new HttpHost(host, port);
 	}
-	
+
 	public void sendEvent(Event event) {
-		// Send the SimpleGeo request here.
-		JSONObject jsonObject = new JSONObject(event.getBody());
-		
+		JSONObject jsonObject = new JSONObject(event.getBody()); 
+		String body;
 		try {
-			String body = jsonObject.getString("body");
+			body = jsonObject.getString("body");
 			// Make sure we have a well formed JSON object
 			body.replaceAll("\\+\"", "\"");
 			jsonObject = new JSONObject(body);
-			
-			String path = jsonObject.getString("path");
-			String method = jsonObject.getString("method");
-			String oauthKey = jsonObject.getString("oauth_consumer_key");
-
-			this.client.getHttpClient().setToken(oauthKey, oauthKey);
-			HttpUriRequest request = null;
-			if(method.equals("GET")) {
-				
-			} else if(method.equals("PUT")) {
-				
-			} else if(method.equals("DELETE")) {
-				
-			} else if(method.equals("POST")) {
-				
-			}
-			
+			sendJSON(jsonObject);
 		} catch (JSONException e) {
 			LOG.warn("Unable to parse JSON event body", e);
-		}		
+		}
 	}
 	
-	private class StupidlySimpleGeoClient extends AbstractSimpleGeoClient {
-
-		public StupidlySimpleGeoClient(String host, int port) {
-			super(host, ""+port, "dontmatter");
-		}
-		
-		@Override
-		protected Object executeDelete(String uri, SimpleGeoJSONHandler handler)
-				throws IOException {
-			;
-			return null;
-		}
-
-		@Override
-		protected void executeDelete(String uri, SimpleGeoJSONHandler handler,
-				SimpleGeoCallback callback) throws IOException {
-			;
-		}
-
-		@Override
-		protected Object executeGet(String uri, SimpleGeoJSONHandler handler)
-				throws IOException {
-			;
-			return null;
-		}
-
-		@Override
-		protected void executeGet(String uri, SimpleGeoJSONHandler handler,
-				SimpleGeoCallback callback) throws IOException {
-			;
-		}
-
-		@Override
-		protected Object executePost(String uri, String jsonPayload,
-				SimpleGeoJSONHandler handler) throws IOException {
-			return null;
-		}
-
-		@Override
-		protected void executePost(String uri, String jsonPayload,
-				SimpleGeoJSONHandler handler, SimpleGeoCallback callback)
-				throws IOException {
-			;
-		}
-
-		@Override
-		protected Object executePut(String uri, String jsonPayload,
-				SimpleGeoJSONHandler handler) throws IOException {
-			;
-			return null;
+	public void sendJSON(JSONObject jsonObject) throws JSONException {
+		BasicHttpRequest request = new BasicHttpRequest(getMethod(jsonObject), getUri(jsonObject));
+		if(request != null) {
+			Map<String, String> headers = getHeaders(jsonObject);
+			if(headers != null)
+				for(String name : headers.keySet())
+					request.setHeader(name, headers.get(name));
+				
+			Map<String, String> arguments = getArguments(jsonObject);
+			if(arguments != null) {
+				HttpParams params = new BasicHttpParams();
+				for(String name : arguments.keySet())
+					params.setParameter(name, arguments.get(name));
+					
+				request.setParams(params);
+			}
+				
+			try {
+				this.client.execute(host, request);
+			} catch (ClientProtocolException e) {
+				LOG.warn("Connection error", e);
+			} catch (IOException e) {
+				LOG.warn("Connection error", e);
+			}
+		} else {
+			LOG.info("Unable to create request for JSON event " +jsonObject.toString());
+		}			
+	}
+	
+	private Map<String, String> getHeaders(JSONObject jsonObject) throws JSONException {
+		return getMap(jsonObject, "headers");
+	}
+	
+	private Map<String, String> getArguments(JSONObject jsonObject) throws JSONException {
+		return getMap(jsonObject, "arguments");
+	}
+	
+	private Map<String, String> getMap(JSONObject jsonObject, String keyName) throws JSONException {
+		HashMap<String, String> map = new HashMap<String, String>();
+		Iterator<String> keyIterator = (Iterator<String>)jsonObject.keys();
+		while(!keyIterator.hasNext()) {
+			String key = keyIterator.next();
+			String[] components = key.split(":");
+			if(components.length > 1 && components[0].equals(keyName)) {
+				map.put(components[1], jsonObject.getString(key));
+			}
 		}
 
-		@Override
-		protected void executePut(String uri, String jsonPayload,
-				SimpleGeoJSONHandler handler, SimpleGeoCallback callback)
-				throws IOException {
-			;
-		}
-		
+		return map;
+	}
+	
+	private String getUri(JSONObject jsonObject) throws JSONException {
+		return jsonObject.getString("path");
+	}
+	
+	private String getMethod(JSONObject jsonObject) throws JSONException {
+		return jsonObject.getString("method");
 	}
 }
--- src/java/com/cloudera/flume/handlers/simplegeo/SimpleGeoReplayDecorator.java
+++ src/java/com/cloudera/flume/handlers/simplegeo/SimpleGeoReplayDecorator.java
@@ -2,11 +2,6 @@ package com.cloudera.flume.handlers.simplegeo;
 
 import java.io.IOException;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.cloudera.flume.conf.Context;
 import com.cloudera.flume.conf.SinkFactory.SinkDecoBuilder;
 import com.cloudera.flume.core.Event;
